7541a169ccba0b790e8b67b1902dc6b1dfc86ba5
Al Sagarino <sagarinoclarin@gmail.com>
2019-06-21 16:43:52 +0800

    add notification functionalities

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { NativeRouter, Route, Switch, BackButton } from "react-router-native";
 import AsyncStorage from "@react-native-community/async-storage";
 import { Platform } from "react-native";
+import firebase from "react-native-firebase";
 
 // import requireAuth from "./src/utils/requireAuth";
 import Login from "./src/Login";
@@ -35,6 +36,44 @@ const client = new ApolloClient({
 });
 
 class App extends Component {
+  componentDidMount() {
+    // Build a channel
+    const channel = new firebase.notifications.Android.Channel(
+      "test-channel",
+      "Test Channel",
+      firebase.notifications.Android.Importance.Max
+    ).setDescription("My apps test channel");
+
+    // Create the channel
+    firebase.notifications().android.createChannel(channel);
+
+    this.removeNotificationDisplayedListener = firebase
+      .notifications()
+      .onNotificationDisplayed(notification => {
+        // Process your notification as required
+        // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+      });
+
+    this.removeNotificationListener = firebase
+      .notifications()
+      .onNotification(notification => {
+        // Process your notification as required
+        this.displayNotification(notification);
+      });
+  }
+
+  componentWillUnmount() {
+    this.removeNotificationDisplayedListener();
+    this.removeNotificationListener();
+  }
+
+  displayNotification = notification => {
+    notification.android
+      .setChannelId("test-channel")
+      .android.setSmallIcon("ic_launcher");
+    firebase.notifications().displayNotification(notification);
+  };
+
   render() {
     return (
       <ApolloProvider client={client}>
--- android/app/src/main/java/com/reactnativeboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativeboilerplate/MainApplication.java
@@ -3,6 +3,10 @@ package com.reactnativeboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseNotificationsPackage(),
+            new RNFirebaseAuthPackage(),
             new AsyncStoragePackage()
       );
     }
